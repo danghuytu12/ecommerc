da5ab44e34eeaae37bd84266de309da1c0bdeaa7
huytu20 <danghuytu.thd2006@gmail.com>
2022-06-19 11:47:24 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {DataProvider} from './GlobalState'
+import Header from './components/headers/Header'
+import MainPages from './components/mainpages/Pages'
+
+
+function App() {
+  return (
+    <div>a</div>
+  );
+}
+
+export default App;
